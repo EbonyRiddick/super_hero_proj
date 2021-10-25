d6bb5fa243ecfab5d3c315b4c66963e0caaab587
Ebony Riddick <ebonyriddick@gmail.com>
2021-10-25 10:03:28 -0400

    added functions to view.py for updating information and deleting a superhero.

--- superhero_project/superheroes/urls.py
+++ superhero_project/superheroes/urls.py
@@ -5,5 +5,7 @@ app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('<int:hero_id>/', views.detail, name ='detail'),
-    path('new/', views.create, name ='create')
+    path('new/', views.create, name ='create'),
+    path('update/', views.update, name ='update'),
+    path('delete/', views.delete, name = 'delete')
 ]
